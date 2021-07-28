a19ba4597e7aeca8ae7582b8f97793bc4bccd6da
Tiago Henrique Faxina <thfaxina@gmail.com>
2021-07-28 16:22:37 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+const port = 3000;
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200)
+        .json(`You are consuming a endpoint with no mTLS requested`);
+});
+
+app.get('/test', (req, res) => {
+    res.status(200)
+        .json(`You are consuming a endpoint ("/test") with no mTLS requested`);
+});
+
+app.get('/webhook', (req, res) => {
+    res.status(200)
+        .json(`Hello ${req.header("ssl_client")}, your certificate was issued by ${req.header("SSL_Client_Issuer")}!`);
+});
+
+app.listen(port, () => {
+  console.log(`.. server up and running and listening on ${port} ..`);
+})
